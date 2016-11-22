6f90f73771671ef2e9902723e19cb63189550fff
zhuxiongw <zhuxiongw@126.com>
2016-11-22 10:31:28 +0800

    activeMQ发送邮件消费者

---
+++ src/main/java/com/vivebest/activemq/consumer/Consumer.java
@@ -0,0 +1,15 @@
+package com.vivebest.activemq.consumer;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+	public static void main(String[] args) {  
+        try {  
+            @SuppressWarnings("resource")
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "classpath:application.xml" });  
+            context.start();  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+    }  
+}
